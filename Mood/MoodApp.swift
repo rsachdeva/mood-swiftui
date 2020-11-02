87fd3ea96ad302ae4a4ca793d34d417f6fdf0700
Rohit Sachdeva <growth@drinnovations.us>
2020-11-01 18:38:27 -0600

    Initial Commit

---
+++ Mood/MoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodApp.swift
+//  Mood
+//
+//  Created by Rohit Sachdeva on 11/1/20.
+//
+
+import SwiftUI
+
+@main
+struct MoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
